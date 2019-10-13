869e7096e92e87986ae07bd379ecddc4d743bcb6
Logan Wright <lwright.js@gmail.com>
2019-10-13 13:31:54 -0500

    adds searching and user profile api

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
+from rest_framework.authentication import TokenAuthentication
 
-from .serializers import HelloSerializer
+from .serializers import HelloSerializer, UserProfileSerializer
+from .models import UserProfile
+from .permissions import UpdateOwnProfile
 
 
 class HelloApiView(APIView):
@@ -87,4 +90,15 @@ class HelloViewSet(viewsets.ViewSet):
     def delete(self, request, pk=None):
         """Handle delete update"""
         return Response({"method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email")
 
